cb1e706f5b16bdc2a4f0bd145418c1d4734d3be5
otaviocesr <rotaviocesar@gmail.com>
2025-04-02 15:42:56 -0300

    Teste de login e Correção de Códigos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); // Importa o body-parser
 const express = require("express"); // Importa biblioteca do express
 const sqlite3 = require("sqlite3"); // Importa biblioteca sqlite3
 
@@ -16,7 +16,7 @@ db.serialize(() => {
 });
 
 // __dirname é variável interna do nodejs que guarda o caminho absoluto do projeto
-console.log(__dirname);
+// console.log(__dirname);
 
 // Aqui será acrescentado uma rota "/static", parar a pasta __dirname + "/static"
 // O app.use é usado para acrescenter rotas novas para o express gerenciar e poder usar
@@ -41,33 +41,78 @@ Na ARROW FUNCTION, o primeiro são os dados do servidor (REQUISITION - 'req')
 O segundo são os dados que serão enviados ao cliente (RESULT - 'res')
 |*/
 app.get("/", (req, res) => {
-  res.render("index");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
+  // res.send(index);
+  console.log("GET /index");
+  // res.render(index);
+  res.redirect("cadastro"); // Redireciona para a ROTA cadastro
 });
 
+// Programação de rotas do método GET do HTTP 'app.get()'
 app.get("/sobre", (req, res) => {
-  res.send(sobre);
+  console.log("GET /sobre");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/sobre
+  res.render("sobre");
 });
 
+// GET Login
 app.get("/login", (req, res) => {
+  console.log("GET /login");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/login
   res.render("login");
 });
 
 // Rota para processar o formulário de login
 app.post("/login", (req, res) => {
+  console.log("POST /login");
   res.send("Login ainda não implementado");
 });
 
+// GET do cadastro
 app.get("/cadastro", (req, res) => {
-  res.send(cadastro);
+  console.log("GET /cadastro");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/cadastro
+  res.render("cadastro");
 });
 
+// POST do cadastro
 app.post("/cadastro", (req, res) => {
-  req.body
-    ? console.log(JSON.stringify(req.body))
-    : console.log(`Body vazio: ${req.bod}`);
-  res.send(
-    `Bem-vindo usuário: ${req.body.username}, seu email é ${req.body.email}`
-  );
+  console.log("POST /cadastro");
+  // Linha para depurar se esta vindo dados no req.body
+  !req.body
+    ? console.log(`Body vazio: ${req.body}`)
+    : console.log(JSON.stringify(req.body));
+
+  const { username, password, email, tel, cpf, rg } = req.body;
+
+  // Colocar aqui as validações e inclusão no banco de dados do cadastro de usuário
+  // 1. Validar dados do usuário
+
+  // 2. Saber se ele já existe no banco
+  const query =
+    "SELECT * from users WHERE email = ? OR cpf = ? OR rg = ? OR username = ?";
+  db.get(query, [email, cpf, rg, username], (err, row) => {
+    if (err) throw err;
+
+    if (row) {
+      // A variável 'row' irá retornar os dados do banco de dados,
+      // executado através do SQL, variável query
+      res.send("Usuário já cadastrado, refaça o cadastro");
+    } else {
+      // 3. Se usuário não existe no banco, cadastrá-lo
+      const insertQuery =
+        "INSERT INTO users (username, password, email, tel, cpf, rg) VALUES (?,?,?,?,?,?)";
+      db.run(insertQuery, [username, password, email, tel, cpf, rg], (err) => {
+        // Inserir a lógica do INSERT
+        if (err) throw err;
+        res.send("Usuário cadastrado com sucesso");
+      });
+    }
+  });
+
+  // res.send(
+  //   `Bem-vindo usuário: ${req.body.username}, seu email é ${req.body.email}`
+  // );
 });
 
 // app.listen() deve ser o último comando da aplicação (app.js)
